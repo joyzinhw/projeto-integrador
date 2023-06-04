d8b07bd6f9dbdc80acbc555f34a2d8353e733c96
joyzinhw <joycemouraifpi@gmail.com>
2023-06-04 15:28:53 -0300

    perfil

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,3 +24,70 @@ panel.setAttribute('aria-expanded', isExpanded);
 
 });
 });
+
+
+// Obtém os elementos do perfil
+var profileInfo = document.getElementById("profileInfo");
+var editButton = document.getElementById("editButton");
+var saveButton = document.getElementById("saveButton");
+var nameSpan = document.getElementById("name");
+var emailSpan = document.getElementById("email");
+var bioSpan = document.getElementById("bio");
+
+// Define um evento para o botão "Editar"
+editButton.addEventListener("click", function() {
+  // Esconde as informações do perfil e exibe o formulário de edição
+  profileInfo.style.display = "none";
+  document.getElementById("profileForm").style.display = "block";
+
+  // Preenche os campos de entrada com as informações do perfil
+  document.getElementById("nameInput").value = nameSpan.textContent;
+  document.getElementById("emailInput").value = emailSpan.textContent;
+  document.getElementById("bioInput").value = bioSpan.textContent;
+});
+
+
+saveButton.addEventListener("click", function() {
+  
+  var name = document.getElementById("nameInput").value;
+  var email = document.getElementById("emailInput").value;
+  var bio = document.getElementById("bioInput").value;
+
+  
+  nameSpan.textContent = name;
+  emailSpan.textContent = email;
+  bioSpan.textContent = bio;
+
+
+  profileInfo.style.display = "block";
+  document.getElementById("profileForm").style.display = "none";
+});
+
+
+nameSpan.textContent = "Nome do Usuário";
+emailSpan.textContent = "usuario@example.com";
+bioSpan.textContent = "Descrição do usuário";
+
+'use strict'
+
+let photo = document.getElementById('imgPhoto');
+let file = document.getElementById('flImage');
+
+photo.addEventListener('click', () => {
+    file.click();
+});
+
+file.addEventListener('change', () => {
+
+    if (file.files.length <= 0) {
+        return;
+    }
+
+    let reader = new FileReader();
+
+    reader.onload = () => {
+        photo.src = reader.result;
+    }
+
+    reader.readAsDataURL(file.files[0]);
+});
