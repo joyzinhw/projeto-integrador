1b734989a1289123d512857bb949fc4d2c2c1d8c
joyzinhw <joycemouraifpi@gmail.com>
2023-06-06 09:54:06 -0300

    pwa

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -1,10 +1,8 @@
-// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
-
 const CACHE = "pwabuilder-offline-page";
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-// TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
+
 const offlineFallbackPage = "/offline.html";
 
 self.addEventListener("message", (event) => {
